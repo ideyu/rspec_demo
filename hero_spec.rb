b178d688f33cc7bc871979253675642cd9f8595d
ideyu <gy4ok4ok@gmail.com>
2015-11-16 16:06:54 +0200

    add power up/down.info test

--- hero_spec.rb
+++ hero_spec.rb
@@ -5,7 +5,26 @@ describe Hero do
   it "has a capitalized name" do 
     hero = Hero.new 'mike'
     
-    expect(hero.name).to eq 'Mik1e' 
+    expect(hero.name).to eq 'Mike' 
   end
+
+  it "can power up" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_up).to eq 110
+  end
+
+  it "can power down" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_down).to eq 90
+  end
+
+  it "displays full hero info" do
+    hero = Hero.new 'mike'
+
+    expect(hero.hero_info).to eq 'Mike has a health of 100'
+  end
+
 
 end
